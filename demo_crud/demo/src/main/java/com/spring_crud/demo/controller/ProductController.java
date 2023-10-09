ab4a989962f8d1dd354af9ed238148e94988a697
User <96388713+aura80@users.noreply.github.com>
2023-10-09 19:37:18 +0300

    basic login added

--- demo_crud/demo/src/main/java/com/spring_crud/demo/controller/ProductController.java
+++ demo_crud/demo/src/main/java/com/spring_crud/demo/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.spring_crud.demo.service.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -12,6 +13,11 @@ public class ProductController {
 
     @Autowired
     private ProductServiceImpl productService;
+
+    @GetMapping("/")
+    public String getMessage(Principal principal) {
+        return "Hello " + principal.getName() + " , I'm a message!";
+    }
 
     @PostMapping("/addProduct")
     public Product addProduct(@RequestBody Product product) {
